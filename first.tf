37e8956cda4db3566efbdc2303132dbb83c03de4
Aamir Meman <aamir.meman@infor.com>
2021-05-05 15:47:29 -0400

    sixth commit

--- first.tf
+++ first.tf
@@ -8,6 +8,6 @@ profile = "account1"
 resource "aws_instance" "example" {
   ami = "ami-0d5eff06f840b45e9"
   instance_type = "t2.micro"
-  subnet_id = "${aws_subnet.us-east-1a-public.id}"
+  subnet_id = "subnet-235c770d"
 }
 
