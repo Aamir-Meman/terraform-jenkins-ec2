ae72ab9b5e64983d94bef4c89a0ffffaa736dacc
Aamir Meman <aamir.meman@infor.com>
2021-05-05 15:33:00 -0400

    sixth commit

--- first.tf
+++ first.tf
@@ -9,8 +9,13 @@ resource "aws_instance" "os2" {
 
 ami = "ami-0d5eff06f840b45e9"
 instance_type = "t2.micro"
+subnet_id = "subnet-235c770d"
 tags = {
   Name = "my first TF OS 12"
   }
 
+}
+resource "aws_eip" "ip" {
+    vpc      = true
+    instance = "${aws_instance.example.id}"
 }
