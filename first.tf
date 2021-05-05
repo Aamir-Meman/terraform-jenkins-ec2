d2ec401311a215c5e27ee1aaea006e53e6b296df
Aamir Meman <aamir.meman@infor.com>
2021-05-05 15:24:14 -0400

    sixth commit

--- first.tf
+++ first.tf
@@ -8,7 +8,7 @@ profile = "account1"
 resource "aws_instance" "os2" {
 
 ami = "ami-0d5eff06f840b45e9"
-instance_type = "t2.small"
+instance_type = "t2.micro"
 tags = {
   Name = "my first TF OS 12"
   }
