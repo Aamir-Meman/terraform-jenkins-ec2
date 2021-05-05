d48db2139786616c5ed512d3ab0d86c876740960
Aamir Meman <aamir.meman@infor.com>
2021-05-05 14:00:35 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# terraform-jenkins-ec2
+This is demo repository for Terraform integration with jenkins.
+# terraform-jenkins-ec2
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region = "${var.region}"
+  secret_key = "nqX9vvYfvfIxWmJXhnhEtCjEHM86/RuQtRDJlmST"
+  access_key = "AKIAXZ25XY73NMXKOF5F"
+}
