68be54a6cb3ad982cdeba5b766dbe490f4a7ef7c
Aamir Meman <aamir.meman@infor.com>
2021-05-05 15:40:25 -0400

    sixth commit

--- first.tf
+++ first.tf
@@ -5,14 +5,20 @@ profile = "account1"
 
 }
 
-resource "aws_instance" "os2" {
+resource "aws_instance" "example" {
+  ami = "ami-0d5eff06f840b45e9"
+  instance_type = "t2.micro"
+  subnet_id = "${aws_subnet.us-east-1a-public.id}"
+}
 
-ami = "ami-0d5eff06f840b45e9"
-instance_type = "t2.micro"
-vpc = true
-subnet_id = "subnet-235c770d"
-tags = {
-  Name = "my first TF OS 12"
-  }
+resource "aws_vpc" "example" {
+  cidr_block = "172.31.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support = true
+}
 
+resource "aws_subnet" "us-east-1a-public" {
+  vpc_id = "${aws_vpc.example.id}"
+  cidr_block = "172.31.80.0/20"
+  availability_zone = "us-east-1a"
 }
