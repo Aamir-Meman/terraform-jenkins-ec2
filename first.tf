db8ea4acb1102469f7b827e53af20f92259e381f
Aamir Meman <aamir.meman@infor.com>
2021-05-05 15:49:50 -0400

    sixth commit

--- first.tf
+++ first.tf
@@ -5,9 +5,28 @@ profile = "account1"
 
 }
 
-resource "aws_instance" "example" {
-  ami = "ami-0d5eff06f840b45e9"
-  instance_type = "t2.micro"
-  subnet_id = "subnet-235c770d"
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
 }
 
